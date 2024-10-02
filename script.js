76aa1cb3f8684dc99cec92ca01f061f4860feb46
sidralimaa <160754094+sidralimaa@users.noreply.github.com>
2024-10-02 07:50:01 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+var image56 = document.getElementById("image56");
+    		if(image56) {
+      			image56.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var groupContainer6 = document.getElementById("groupContainer6");
+    		if(groupContainer6) {
+      			groupContainer6.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var lOJAText = document.getElementById("lOJAText");
+    		if(lOJAText) {
+      			lOJAText.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var mINHACONTAText = document.getElementById("mINHACONTAText");
+    		if(mINHACONTAText) {
+      			mINHACONTAText.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var cONTATOText = document.getElementById("cONTATOText");
+    		if(cONTATOText) {
+      			cONTATOText.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
